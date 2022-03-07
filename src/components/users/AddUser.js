9f2eb4b8890107f7d3125a3faa180fc84fb8e2ee
satvinder <satvinder.singh@digimantra.com>
2022-03-07 13:29:07 +0530

    fix: Changes Done

---
+++ src/components/users/AddUser.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",  
+    phone: "",
+    website: ""
+  });
+  const [error, setError] = useState('')
+  const [nameError, setErrorName] = useState('')
+  const { name, username, email, phone, website } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if(!name || !username || !email || !phone || !website){
+      setError('Please required')
+    }else if(!name.match('^[a-zA-Z ]*$')){
+      setErrorName('Please Only Charchter')
+    }
+    else{
+      await axios.post("http://localhost:3000/users", user);
+      history.push("/");
+      setError('')
+    }
+   
+  };
+  const handleBlur = () => {
+    if (!name.match('^[a-zA-Z ]*$')) {
+      // Send an API request to check if email is already taken.
+      setErrorName('Please Only Charchter')
+    } else {
+      setErrorName(true);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add A User</h2>
+        <span style={{color:'red',textAlign:'center'}}>{error&&error}</span>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onBlur={handleBlur}
+              onChange={e => onInputChange(e)}
+            />
+            {nameError&&nameError}
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Username"
+              name="username"
+              value={username}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              value={phone}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Website Name"
+              name="website"
+              value={website}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block">Add User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
